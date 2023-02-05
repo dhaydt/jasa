b5b43b3cbb6ce9b71732e117e9ce65b096db8eaf
dhaydt <1992dayat@gmail.com>
2023-02-05 19:38:59 +0700

    fix not readeble city

--- resources/views/frontend/partials/footer.blade.php
+++ resources/views/frontend/partials/footer.blade.php
@@ -107,8 +107,8 @@ $footer_variant = !is_null(get_footer_style()) ? get_footer_style() : '02';
                     if(loaded == ''){
                         location.reload();
                     }
-                    if(response == 'no_services' && Session::get('city_id') != null){
-                        location.reload();
+                    if(response == 'no_services' && '{{ Session::get('city_id') }}' != null){
+                        location.reload()
                     }
                     if(city_old != city_new && response == 'empty'){
                         location.reload();
