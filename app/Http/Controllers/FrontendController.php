8d105599bb080396872e38824de0618c7eb0627f
dhaydt <1992dayat@gmail.com>
2023-02-06 13:28:12 +0700

    test ipapi api

--- app/Http/Controllers/FrontendController.php
+++ app/Http/Controllers/FrontendController.php
@@ -36,8 +36,10 @@ class FrontendController extends Controller
         // session()->forget('city_name');
 
         $ip = $_SERVER['REMOTE_ADDR'];
-        $details = json_decode(file_get_contents("http://ipinfo.io/{$ip}/json"));
+        // $details = json_decode(file_get_contents("http://ipinfo.io/{$ip}/json"));
+        $details = json_decode(file_get_contents("https://ipapi.co/{$ip}/json/"));
         $details->city = 'jakarta';
+        dd($details);
         if (isset($details->city)) {
             $city_name = $details->city;
             $city = ServiceCity::where('service_city', $city_name)->first();
