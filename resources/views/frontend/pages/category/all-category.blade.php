0274809a128010a203719400cdc5163bb54f1513
dhaydt <1992dayat@gmail.com>
2023-02-12 11:16:31 +0700

    fix img size all categories

--- resources/views/frontend/pages/category/all-category.blade.php
+++ resources/views/frontend/pages/category/all-category.blade.php
@@ -20,13 +20,13 @@
         </div>
         <div class="row margin-top-20">
             @foreach($all_category as $cat)
-            <div class="col-xl-2 col-lg-3 col-sm-6 margin-top-30 category-child">
+            <div class="col-xl-1 col-lg-1 col-sm-4 col-4 margin-top-30 category-child">
                 <div class="single-category style-02 wow fadeInUp" data-wow-delay=".2s" style="visibility: visible; animation-delay: 0.2s; animation-name: fadeInUp;">
                     <div class="icon">
                         {!! render_image_markup_by_attachment_id($cat->image,'','','thumb'); !!}
                     </div>
                     <div class="category-contents">
-                        <h4 class="category-title"> <a href="{{ route('service.list.category',$cat->slug) }}"> {{ $cat->name }} </a> </h4>
+                        <h4 class="category-title"> <a href="{{ route('service.list.category',$cat->slug) }}"> {{ $cat->name }}</a> </h4>
                         {{-- <span class="category-para"> {{ $cat->services->count() }}+ {{ __('Service') }} </span> --}}
                     </div>
                 </div>
