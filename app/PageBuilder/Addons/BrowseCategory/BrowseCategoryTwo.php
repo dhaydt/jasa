d41046447bf20f1d1f78b44812d35c33d43572e3
dhaydt <1992dayat@gmail.com>
2023-02-26 22:16:00 +0700

    style card without col

--- app/PageBuilder/Addons/BrowseCategory/BrowseCategoryTwo.php
+++ app/PageBuilder/Addons/BrowseCategory/BrowseCategoryTwo.php
@@ -139,7 +139,7 @@ class BrowseCategoryTwo extends \App\PageBuilder\PageBuilderBase
             $service_count = $cat->services->count();
 
  $category_markup.= <<<CATEGORY
-    <div class="category-child col-3 col-md-2 mt-2">
+    <div class="category-child mt-2">
         <a href="{$route}/{$slug}" class="single-category style-02 wow fadeInUp" data-wow-delay=".2s">
                 <div class="icon category-bg-thumb-format"  {$category_image}>
                 </div>
